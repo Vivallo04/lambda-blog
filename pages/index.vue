25e51c3b84f0d61cbae8486552c667c1ef325b27
Vivallo <andresvivallo4@gmail.com>
2022-06-04 21:05:56 -0600

    General file changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <Tutorial />
+  <container>
+    Hello World
+    <NuxtLogo />
+  </container>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
+import Default from '~/layouts/default.vue';
+import NuxtLogo from '~/components/NuxtLogo.vue';
 
 export default Vue.extend({
-  name: 'IndexPage'
-})
+  name: 'IndexPage',
+  components: { Default, NuxtLogo }
+});
 </script>
