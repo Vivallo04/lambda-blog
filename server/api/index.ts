3ce7cc4db1467329318b2f79ecb7801b49ec68ee
Vivallo <andresvivallo4@gmail.com>
2022-06-05 23:06:35 -0600

    Add API

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,8 +1,20 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 
 
 const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
+
+console.log("Server is running");
+
+app.post(`/logs`, async (req, res) => {
+    
+});
+
+
+export default {
+  path: '/api',
+  handler: app
+};
