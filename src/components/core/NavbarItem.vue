6a24e4522c53d65a2d8f84dcd1929664726fdac2
Vivallo <andresvivallo4@gmail.com>
2022-07-05 12:33:11 -0600

    Add Tailwind

---
+++ src/components/core/NavbarItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  label: string,
+}>();
+</script>
+
+<template>
+  <div class="item">
+    <div class="details">
+      <span>
+        <slot name="link"></slot>
+         <a target="_blank" href="https://vitejs.dev/">{{ label }}</a>
+      </span>
+    </div>
+  </div>
+</template>
+
+
+<style>
+
+</style>
